e9fe918bee2f52afd20406cd211378d4bd106ec7
fahadq123 <42976252+fahadq123@users.noreply.github.com>
2019-01-01 05:20:16 -0500

    description about the project
    you can do the following:
    1. count the characters in a message
    2. capitalize the first character of words
    3. reverse a message's words in place
    4. reversing characters in place
    5 .replace the first occurrence of a string
    6. replace all occurrences of a string
    7. encode text by swapping certain characters for other characters

---
+++ numberMixer.js
@@ -0,0 +1,8 @@
+// in this project you can do the following : 
+//count the characters in a message
+//capitalize the first character of words
+//reverse a message's words in place
+//reversing characters in place
+//replace the first occurrence of a string
+//replace all occurrences of a string
+//encode text by swapping certain characters for other characters
